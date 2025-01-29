7709dcf3e027a0e2b4bc5966f43f6867836a7f72
Mehdi <surface@builgDiff.local>
2025-01-28 23:45:19 -0500

    getting the id of created background tab

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,38 +1,89 @@
 injectContentScript = (tab) => {
     const {id, url} = tab;
+
     chrome.scripting.executeScript(
       {
         target: {tabId: id, allFrames: true},
         files: ['/scripts/book-scraping.js']
       }
     )
+    
   }
 
 getCurrentTab = async () => {
     let queryOptions = { active: true };
+
     let [tab] = await chrome.tabs.query(queryOptions);
+
     return tab;
+
 }
 
 getCurrentTab().then((tab)=>{
     injectContentScript(tab)
+
   })
+
+newHiddenTab = async (url) => {
+    let hiddenTab = await chrome.tabs.create({url: url, active:false});
+
+    return hiddenTab;
+
+}
+
+updateHiddenTab = (url, tabId) => {
+    chrome.tabs.update({url: url, tabId: tabId})
+
+}
+
+deleteHiddenTab = (tabId) => {
+  // chrome.tabs.remove({tabId:"id"})
+
+}
 
 // message handling
 //--------------------------------------------------------------
   
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  async function(request, sender, sendResponse) {
     if (request.code === 200){
       var pdfList = document.getElementById('pdfList');
+
       request.urls.forEach(element => {
         var listItem = document.createElement('li');
+
         listItem.textContent = element;
+
         pdfList.appendChild(listItem);
+
       });
     }
-    else{
-      alert("There has been an error in gathering pdf urls please reload the page !")
+    
+    // replace with swich
+    else if (request.code === 201){
+      newHiddenTab(request.url).then( async (hiddenTab) => {
+        try {
+            alert(hiddenTab.id);
+
+            //sending the id of the new hiddenTab
+            const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+
+            const response = await chrome.tabs.sendMessage(tab.id, {code: 204, hiddenTabId: hiddenTab});
+
+        } catch (error) {
+            alert("There has been an error in gathering pdf urls please reload the page ! Error when displaying gathered pdfList")
+        }
+      })
     }
+    
+    else if (request.code === 202){
+        updateHiddenTab(request.url, request.tabId);
+
     }
+
+    else if (request.code === 203){
+        deleteHiddenTab(request.tabId);
+
+    }
+  }
 );
